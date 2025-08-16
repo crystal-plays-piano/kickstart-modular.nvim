118c5e1ce10ff44dfe2e5dc53e52ad1fbbe0ffbb
crystal mandal <user@legion-archlinux>
2025-08-16 00:46:41 -0500

    Sat, 16 Aug 2025 00:46:41 -0500

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- -- THIS IS FOR LILYPOND SYNTAC SYNC
+  -- vim.api.nvim_create_autocmd('BufEnter', {
+  --   command = 'syntax sync fromstart',
+  --   pattern = { '*.ly', '*.ily', '*.tex' },
+  -- }),
+}
