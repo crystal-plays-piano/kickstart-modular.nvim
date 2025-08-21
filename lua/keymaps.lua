a09c5486e18a23e3a52043eb2622d5f6f070f1df
crystal mandal <user@legion-archlinux>
2025-08-21 18:53:31 -0500

    Thu, 21 Aug 2025 18:53:31 -0500

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,7 +4,10 @@
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
+vim.o.tabstop = 2
+vim.o.expandtab = true
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
